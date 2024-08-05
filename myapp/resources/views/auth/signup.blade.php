eb1ca1cf13b2ab2bd0d1e03c72d9a5c0da12570e
jack <gtaped14876@gmail.com>
2024-08-05 15:21:41 +0800

    將錯誤訊息獨立寫成一個blade 成功登入存session (預計到首頁) 登入失敗導向到登入頁面

--- myapp/resources/views/auth/signup.blade.php
+++ myapp/resources/views/auth/signup.blade.php
@@ -37,17 +37,7 @@
 				<div class="container">
 
 					<div >
-						<div class="form-result">
-						@if( $errors and count($errors))
-							<ul class="iconlist" data-username="envato" data-count="2">
-							@foreach( $errors->all() as $err )
-								<div class="alert text-center alert-danger">
-								{{$err}} 
-								</div>
-							@endforeach
-							</ul>
-						@endif 
-						</div>
+						@include('components.errorMessage')
 
 						<div class="row">
 							<div class="col-lg-12">
