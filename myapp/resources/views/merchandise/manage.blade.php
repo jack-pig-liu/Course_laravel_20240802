a1d42d20414b914dcb6faf51f01420e4da7e8920
jack <gtaped14876@gmail.com>
2024-08-06 15:47:49 +0800

    顯示圖片時加上長寬 避免畫面不協調

--- myapp/resources/views/merchandise/manage.blade.php
+++ myapp/resources/views/merchandise/manage.blade.php
@@ -25,7 +25,7 @@
                             <td> {{ $Merchandise->id }}</td>
                             <td> {{ $Merchandise->name }}</td>
                             <td>
-                                <img src="{{ $Merchandise->photo }}" />
+                                <img src="{{ asset($Merchandise->photo) }}" width='100' height='100'/>
                             </td>
                             <td>
                                 @if($Merchandise->status == 'C')
